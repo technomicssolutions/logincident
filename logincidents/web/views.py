d63dc2eaeb9418abb1e4cebb01e30054560f4589
remya <remya@remya.(none)>
2013-11-18 13:14:42 +0530

    home page

--- logincidents/web/views.py
+++ logincidents/web/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import HttpResponse,redirect
+from django.shortcuts import render_to_response,get_object_or_404
+
+
+
+def home(request):
+    c={'user':request.user}
+    return render_to_response('home.html',c)
